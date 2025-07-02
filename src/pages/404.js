7331da4e36c0069d9d0f2afc2872e0007a082183
dondoko_susumu <kkbt2003@gmail.com>
2025-07-02 16:53:31 +0900

    日本語に対応するための言語設定を変更し、404ページとホームページのコンテンツを日本語に更新。受賞歴やインタビュー情報を追加し、SNSリンクを整理。

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,14 +2,35 @@ import * as React from "react"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
 
 const NotFoundPage = () => (
   <Layout>
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <h1>いや、どこ見とんねん</h1>
+    <div>
+      <Link
+        to="/"
+        style={{
+          fontSize: `var(--font-sm)`,
+          textDecoration: `none`,
+        }}
+      >
+      こっこから戻りや
+      </Link>
+    </div>
+    <StaticImage
+      src="../images/dondoko_susumu.jpg"
+      loading="eager"
+      width={300}
+      quality={95}
+      formats={["auto", "webp", "avif"]}
+      alt=""
+      style={{ marginBottom: `var(--space-3)` }}
+    />
   </Layout>
 )
 
-export const Head = () => <Seo title="404: Not Found" />
+export const Head = () => <Seo title="いや、どこにアクセスしとんねん" />
 
 export default NotFoundPage
