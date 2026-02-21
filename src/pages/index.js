394b45b98d2f2afcbc5a1c143a93281987578951
dondoko_susumu <kkbt2003@gmail.com>
2026-02-21 15:21:59 +0900

    作品リスト、受賞歴、インタビュー、SNS、連絡セクションを追加し、スタイルを整えました

--- src/pages/index.js
+++ src/pages/index.js
@@ -32,37 +32,37 @@ const oneshots = [
       "不思議な力を持った涙を流す巨大なおじさんの話。",
   },
   {
-    text: "2024.07 「最期の火花」,",
+    text: "2024.07 「最期の火花」",
     url: "https://www.pixiv.net/artworks/121053801#1",
     description:
       "死の間際に瀕した父親が最期に想い浮かべた人物とは...",
   },
   {
-    text: "2024.07 「河川敷レトロ」,",
+    text: "2024.07 「河川敷レトロ」",
     url: "https://www.pixiv.net/artworks/120491303#1",
     description:
       "強い女の子と弱い男の子の話。",
   },
   {
-    text: "2024.06 「親子ドリブル」,",
+    text: "2024.06 「親子ドリブル」",
     url: "https://www.pixiv.net/artworks/119445862#1",
     description:
       "子供のサッカーに過剰に干渉する親の話。",
   },
   {
-    text: "2024.05 「サムライと漫画家志望」,",
+    text: "2024.05 「サムライと漫画家志望」",
     url: "https://www.pixiv.net/artworks/116829152#1",
     description:
       "元プロボクサーの父親とプロレス大好きな息子が対決する話。",
   },
   {
-    text: "2024.03 「鬼っこレスラー」,",
+    text: "2024.03 「鬼っこレスラー」",
     url: "https://www.pixiv.net/artworks/118335945#1",
     description:
       "女の子を描きたくない漫画家志望の話。",
   },
   {
-    text: "2023.01 「Satellite - サテライト -」,",
+    text: "2023.01 「Satellite - サテライト -」",
     url: "https://www.pixiv.net/artworks/104687029#1",
     description:
       "年に1点しかゴールを決められないフォワードの話。",
@@ -84,15 +84,15 @@ const etcLinks = [
     description:
       "リーゼント頭をした次世代型AIロボットどんどこすすむくん。子供向け漫画。",
   },
-  // {
-  //   text: "2023.07 コルクマンガ専科",
-  //   url: "https://www.pixiv.net/user/1758/series/211385",
-  //   description:
-  //     "コルクマンガ専科の課題で描いた4コマ漫画など。",
-  // },
+  {
+    text: "2023.07 コルクマンガ専科",
+    url: "https://www.pixiv.net/user/1758/series/211385",
+    description:
+      "コルクマンガ専科の課題で描いた4コマ漫画など。",
+  },
   {
     text: "2020.07〜 どんどこすすむダジャレイラスト",
-    url: "https://www.pixiv.net/user/1758/series/211385",
+    url: "https://www.pixiv.net/tags/%E3%81%A9%E3%82%93%E3%81%A9%E3%81%93%E3%81%99%E3%81%99%E3%82%80%E3%83%80%E3%82%B8%E3%83%A3%E3%83%AC/artworks",
     description:
       "どんどこすすむダジャレイラストシリーズ。不定期更新中。",
   },
@@ -149,6 +149,12 @@ const interviewlinks = [
     url: "https://realsound.jp/book/2024/09/post-1792381.html",
     description:
       "",
+  },
+  {
+    text: "2019.04 エンジニア時代のインタビュー",
+    url: "https://www.wantedly.com/companies/linksports/post_articles/128004",
+    description:
+      "おまけ。エンジニア時代の貴重なインタビュー。",
   },
 ]
 
@@ -268,112 +274,122 @@ const IndexPage = () => (
         現在、バイトをしながら専業漫画家を目指している<br/>
       </p>
     </div>
-    <h3>作品一覧</h3>
-    <h4>読切</h4>
-    <ul className={styles.list}>
-      {oneshots.map(link => (
-        <li key={link.url} className={styles.listItem}>
-          <a
-            className={styles.listItemLink}
-            href={`${link.url}${utmParameters}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {link.text}
-          </a>
-          {link.description && (
-            <p className={styles.listItemDescription}>
-              {link.description}
-            </p>
-          )}
-        </li>
-      ))}
-    </ul>
-    <h4>その他の漫画やイラスト</h4>
-    <ul className={styles.list}>
-      {etcLinks.map(link => (
-        <li key={link.url} className={styles.listItem}>
-          <a
-            className={styles.listItemLink}
-            href={`${link.url}${utmParameters}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {link.text}
-          </a>
-          {link.description && (
-            <p className={styles.listItemDescription}>
-              {link.description}
-            </p>
-          )}
-        </li>
-      ))}
-    </ul>
-    <h3>受賞歴</h3>
-    <ul className={styles.list}>
-      {awardlinks.map(link => (
-        <li key={link.url} className={styles.listItem}>
-          <a
-            className={styles.listItemLink}
-            href={`${link.url}${utmParameters}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {link.text}
-          </a>
-          {link.description && (
-            <p className={styles.listItemDescription}>
-              {link.description}
-            </p>
-          )}
-        </li>
-      ))}
-    </ul>
-    <h3>インタビュー</h3>
-    <ul className={styles.list}>
-      {interviewlinks.map(link => (
-        <li key={link.url} className={styles.listItem}>
-          <a
-            className={styles.listItemLink}
-            href={`${link.url}${utmParameters}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {link.text}
-          </a>
-          <p className={styles.listItemDescription}>{link.description}</p>
-        </li>
-      ))}
-    </ul>
-    <h3>SNS</h3>
-    <ul className={styles.list}>
-      {links.map(link => (
-        <li key={link.url} className={styles.listItem}>
-          <a
-            className={styles.listItemLink}
-            href={`${link.url}${utmParameters}`}
-            target="_blank"
-            rel="noreferrer"
-          >
-            {link.text}
-          </a>
-          {link.description && (
-            <p className={styles.listItemDescription}>
-              {link.description}
-            </p>
-          )}
-        </li>
-      ))}
-    </ul>
-    <h3>連絡</h3>
-    <a
-      className={styles.listItemLink}
-      href={`mailto:kkbt2003@gmail.com`}
-      target="_blank"
-      rel="noreferrer"
-    >
-      kkbt2003@gmail.com
-    </a>
+    <div className={styles.section}>
+      <h3>作品リスト</h3>
+      <h4>読切</h4>
+      <ul className={styles.list}>
+        {oneshots.map(link => (
+          <li key={link.url} className={styles.listItem}>
+            <a
+              className={styles.listItemLink}
+              href={`${link.url}${utmParameters}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {link.text}
+            </a>
+            {link.description && (
+              <p className={styles.listItemDescription}>
+                {link.description}
+              </p>
+            )}
+          </li>
+        ))}
+      </ul>
+      <h4>その他の漫画やイラスト</h4>
+      <ul className={styles.list}>
+        {etcLinks.map(link => (
+          <li key={link.url} className={styles.listItem}>
+            <a
+              className={styles.listItemLink}
+              href={`${link.url}${utmParameters}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {link.text}
+            </a>
+            {link.description && (
+              <p className={styles.listItemDescription}>
+                {link.description}
+              </p>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+    <div className={styles.section}>
+      <h3>受賞歴</h3>
+      <ul className={styles.list}>
+        {awardlinks.map(link => (
+          <li key={link.url} className={styles.listItem}>
+            <a
+              className={styles.listItemLink}
+              href={`${link.url}${utmParameters}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {link.text}
+            </a>
+            {link.description && (
+              <p className={styles.listItemDescription}>
+                {link.description}
+              </p>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+    <div className={styles.section}>
+      <h3>インタビュー</h3>
+      <ul className={styles.list}>
+        {interviewlinks.map(link => (
+          <li key={link.url} className={styles.listItem}>
+            <a
+              className={styles.listItemLink}
+              href={`${link.url}${utmParameters}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {link.text}
+            </a>
+            <p className={styles.listItemDescription}>{link.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+    <div className={styles.section}>
+      <h3>SNS</h3>
+      <ul className={styles.list}>
+        {links.map(link => (
+          <li key={link.url} className={styles.listItem}>
+            <a
+              className={styles.listItemLink}
+              href={`${link.url}${utmParameters}`}
+              target="_blank"
+              rel="noreferrer"
+            >
+              {link.text}
+            </a>
+            {link.description && (
+              <p className={styles.listItemDescription}>
+                {link.description}
+              </p>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
+    <div className={styles.section}>
+      <h3>連絡</h3>
+      <a
+        className={styles.listItemLink}
+        href={`mailto:kkbt2003@gmail.com`}
+        target="_blank"
+        rel="noreferrer"
+      >
+        kkbt2003@gmail.com
+      </a>
+    </div>
   </Layout>
 )
 
